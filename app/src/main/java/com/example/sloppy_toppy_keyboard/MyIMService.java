2ae5a648a1dc95dd29a9aa074917a59e6eb2f70d
Michael McMasters <jmcmasters411@gmail.com>
2021-08-30 22:10:08 -0400

    Make button appear as keyboard

---
+++ app/src/main/java/com/example/sloppy_toppy_keyboard/MyIMService.java
@@ -0,0 +1,31 @@
+package com.example.sloppy_toppy_keyboard;
+
+import android.inputmethodservice.InputMethodService;
+import android.view.View;
+import android.view.inputmethod.InputConnection;
+import android.widget.Button;
+
+public class MyIMService extends InputMethodService implements View.OnClickListener {
+
+    private static String TAG = "MyIMService";
+
+    @Override
+    public View onCreateInputView() {
+        View myKeyboardView = getLayoutInflater().inflate(R.layout.key_layout, null);
+        Button btnA = myKeyboardView.findViewById(R.id.btnA);
+        btnA.setOnClickListener(this);
+        //ADD ALL THE OTHER LISTENERS HERE FOR ALL THE KEYS
+        return myKeyboardView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        //handle all the keyboard key clicks here
+
+        InputConnection ic = getCurrentInputConnection();
+        if (v instanceof Button) {
+            String clickedKeyText = ((Button) v).getText().toString();
+            ic.commitText(clickedKeyText, 1);
+        }
+    }
+}
