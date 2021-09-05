78afff5d5d81a8d240caedd1391989864b3e475f
Michael McMasters <jmcmasters411@gmail.com>
2021-09-05 00:30:39 -0400

    Calculate angle of touch from button

--- app/src/main/java/com/example/sloppy_toppy_keyboard/MyIMService.java
+++ app/src/main/java/com/example/sloppy_toppy_keyboard/MyIMService.java
@@ -1,5 +1,6 @@
 package com.example.sloppy_toppy_keyboard;
 
+import android.graphics.Point;
 import android.inputmethodservice.InputMethodService;
 import android.util.Log;
 import android.view.MotionEvent;
@@ -37,61 +38,38 @@ public class MyIMService extends InputMethodService implements View.OnClickListe
 
         View.OnTouchListener onTouchListenerCallback = (view, motion) -> {
             motion.getOrientation();
-            //float position = motion.getX(0);
-            //Log.d(TAG, "onCreateInputView: " + position);
 
-            //Vector2 viewCenterVector = new Vector2(view.getX() + (view.getWidth() * 0.5f), view.getY() + view.getHeight() * 0.5f);
-            Vector2 viewCenterVector = new Vector2(view.getWidth() * 0.5f, view.getHeight() * 0.5f);
-            Vector2 touchVector = new Vector2(motion.getX(), motion.getY());
+            // (0, 0) plus width and height halved to get center of button.
+            Vector2 buttonCenterPos = new Vector2(view.getWidth() * 0.5f, view.getHeight() * 0.5f);
+            Vector2 touchRelativePos = new Vector2(motion.getX(), motion.getY());
+            double touchDistFromCenter = Math.hypot(buttonCenterPos.x - touchRelativePos.x, touchRelativePos.y - buttonCenterPos.y);
 
-            double distance = Math.hypot(viewCenterVector.x - touchVector.x, touchVector.y - viewCenterVector.y);
+            // Plan: If touch is > than x distance away, get angle and insert that character.
 
-            if (distance > -40 && distance < 40) {
-                ic.commitText("hiya", 1);
-            }
-            Log.d(TAG, "onCreateInputView: " + distance);
+//            if (touchDistFromCenter > -40 && touchDistFromCenter < 40) {
+//                ic.commitText("hiya", 1);
+//            }
+            Log.d(TAG, "onCreateInputView: " + touchDistFromCenter);
             //Log.d(TAG, "onCreateInputView: " + touchVector.x + " " + touchVector.y);
-            Log.d(TAG, "onCreateInputView: " + viewCenterVector.x + " " + viewCenterVector.y);
+            //Log.d(TAG, "onCreateInputView: " + buttonCenterPos.x + " " + buttonCenterPos.y);
 
+            float angle = getAngle(buttonCenterPos, touchRelativePos);
+            Log.d(TAG, "onCreateInputView: Relative Pos: " + angle);
 
-            int action = motion.getActionMasked();
-            switch(action) {
-                case (MotionEvent.ACTION_HOVER_ENTER):
-                    Log.d(TAG, "action hover enter");
-                    return true;
-                case (MotionEvent.ACTION_OUTSIDE):
-                    Log.d(TAG, "action not outsie");
-                    return true;
-                case (MotionEvent.AXIS_RELATIVE_X):
-                    Log.d(TAG, "action not outsie");
-                    return true;
-                default:
-                    return true;
-            }
-
-//            float position = motion.getRawX(0);
-//            Log.d(TAG, String.valueOf(position));
-//            Vector2 vector = new Vector2();
-//            Vector v;
+            return true;
         };
 
         button0.setOnTouchListener(onTouchListenerCallback);
         button1.setOnTouchListener(onTouchListenerCallback);
-
-
-//        button1.setOnTouchListener((view, motion) -> {
-////            onGenericMotionEvent()
-//            Log.d(TAG, "onCreateInputView: view");
-//            Log.d(TAG, "onCreateInputView: motion");
-//            return true;
-//        });
-//        button2.setOnHoverListener((view, motion) -> {
-//            Log.d(TAG, "onCreateInputView: view");
-//            Log.d(TAG, "onCreateInputView: motion");
-//            return true;
-//        });
-        //ADD ALL THE OTHER LISTENERS HERE FOR ALL THE KEYS
         return myKeyboardView;
+    }
+
+    private float getAngle(Vector2 target, Vector2 other) {
+        float angle = (float) Math.toDegrees(Math.atan2(target.y - other.y, target.x - other.x));
+        if(angle < 0)
+            angle += 360;
+
+        return angle;
     }
 
     @Override
