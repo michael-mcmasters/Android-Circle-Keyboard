7110e86ec1247890256364484ac82e0743693b26
Michael McMasters <jmcmasters411@gmail.com>
2021-09-05 00:49:52 -0400

    Letters are only inputted when user drags back to center of button

--- app/src/main/java/com/example/sloppy_toppy_keyboard/MyIMService.java
+++ app/src/main/java/com/example/sloppy_toppy_keyboard/MyIMService.java
@@ -1,9 +1,7 @@
 package com.example.sloppy_toppy_keyboard;
 
-import android.graphics.Point;
 import android.inputmethodservice.InputMethodService;
 import android.util.Log;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.inputmethod.InputConnection;
 import android.widget.Button;
@@ -11,6 +9,8 @@ import android.widget.Button;
 public class MyIMService extends InputMethodService implements View.OnClickListener {
 
     private static String TAG = "MyIMService";
+
+    private String selectedLetter = "";
 
     private static class Vector2 {
         private double x;
@@ -31,7 +31,7 @@ public class MyIMService extends InputMethodService implements View.OnClickListe
         button0.setOnClickListener(this);
         button1.setOnClickListener(this);
 
-        InputConnection ic = getCurrentInputConnection();
+        InputConnection inputConnection = getCurrentInputConnection();
 
         View.OnTouchListener onTouchListenerCallback = (view, motion) -> {
                                           // (0, 0) plus width and height halved to get center of button.
@@ -42,13 +42,24 @@ public class MyIMService extends InputMethodService implements View.OnClickListe
             // If touch is > than x distance away, get angle and insert that character.
             if (touchDistFromCenter > 40) {
                 float angle = getAngle(buttonCenterPos, touchRelativePos);
-                if (angle > 80 && angle < 100) {
-                    ic.commitText("a", 1);
+                if (isInRange(angle, 0)) {
+                    selectedLetter = "a";
                 }
-                else if (angle > 260 && angle < 280) {
-                    ic.commitText("z", 1);
+                else if (isInRange(angle, 90)) {
+                    selectedLetter = "b";
+                }
+                else if (isInRange(angle, 180)) {
+                    selectedLetter = "y";
+                }
+                else if (isInRange(angle, 270)) {
+                    selectedLetter = "z";
                 }
             }
+            else if (touchDistFromCenter < 30 && selectedLetter != "") {
+                inputConnection.commitText(selectedLetter, 1);
+                selectedLetter = "";
+            }
+
             Log.d(TAG, "onCreateInputView: " + touchDistFromCenter);
             //Log.d(TAG, "onCreateInputView: " + touchVector.x + " " + touchVector.y);
             //Log.d(TAG, "onCreateInputView: " + buttonCenterPos.x + " " + buttonCenterPos.y);
@@ -69,6 +80,12 @@ public class MyIMService extends InputMethodService implements View.OnClickListe
             angle += 360;
 
         return angle;
+    }
+
+    private boolean isInRange(float angle, int range) {
+        if (angle > range - 10 && angle < range + 10)
+            return true;
+        return false;
     }
 
     @Override
