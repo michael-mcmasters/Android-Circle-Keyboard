88a77eaef2007af940f297fdbd1cef53b952905d
Michael McMasters <jmcmasters411@gmail.com>
2024-01-27 02:10:40 -0500

    Move code into readable methods

--- app/src/main/java/com/example/sloppy_toppy_keyboard/ButtonListener.java
+++ app/src/main/java/com/example/sloppy_toppy_keyboard/ButtonListener.java
@@ -16,14 +16,8 @@ public class ButtonListener {
     private CircleKeyboardApplication circleKeyboardApplication;
     private MainKeyboardView mainKeyboardView;
 
-    private String selectedLetter = "";
-    private String prevSelectedLetter = "";
-    private static final int firstRingActivationRange = 85;
-    private static final int secondRingActivationRange = 200;
-
     private Vector2 startPosition;
     private Vector2 endPosition;
-
 
 
     public ButtonListener(Context context, CircleKeyboardApplication circleKeyboardApplication, MainKeyboardView mainKeyboardView) {
@@ -39,42 +33,49 @@ public class ButtonListener {
         return (view, motionEvent) -> {
             Integer fingerAction = MotionEventCompat.getActionMasked(motionEvent);
             if (fingerAction.equals(MotionEvent.ACTION_DOWN)) {
-                Log.d(TAG, "ACTION_DOWN");
-                startPosition = new Vector2(motionEvent.getX(0), motionEvent.getY(0));  // should be relative to the button
+                Log.d(TAG, "Touch Down");
+                onTouchDown(motionEvent);
             } else if (fingerAction.equals(MotionEvent.ACTION_UP)) {
-                Log.d(TAG, "ACTION_UP");
-                endPosition = new Vector2(motionEvent.getX(0), motionEvent.getY(0));
-
-                Log.d(TAG, String.format("startPosition x: %s, y: %s", startPosition.x, startPosition.y));
-                Log.d(TAG, String.format("endPosition x: %s, y: %s", endPosition.x, endPosition.y));
-                Log.d(TAG, "");
-
-                double xDistance = Math.abs(startPosition.x - endPosition.x);
-                double yDistance = Math.abs(startPosition.y - endPosition.y);
-
-                // Determine if trail was longer left/right or up/down to determine which letter to prioritize. (Useful if moving diagonally.)
-                if (xDistance > yDistance) {
-                    if (endPosition.x < startPosition.x) {
-                        // left
-                        circleKeyboardApplication.commitText("L");
-                    } else {
-                        // right
-                        circleKeyboardApplication.commitText("R");
-                    }
-                } else {
-                    if (endPosition.y < startPosition.y) {
-                        // up
-                        circleKeyboardApplication.commitText("U");
-                    } else {
-                        // down
-                        circleKeyboardApplication.commitText("D");
-                    }
-                }
+                Log.d(TAG, "Touch Up");
+                onTouchUp(motionEvent);
             }
 
             view.performClick();
             return true;
         };
+    }
+
+    private void onTouchDown(MotionEvent motionEvent) {
+        startPosition = new Vector2(motionEvent.getX(0), motionEvent.getY(0));  // should be relative to the button
+    }
+
+    private void onTouchUp(MotionEvent motionEvent) {
+        endPosition = new Vector2(motionEvent.getX(0), motionEvent.getY(0));
+        Log.d(TAG, String.format("startPosition x: %s, y: %s", startPosition.x, startPosition.y));
+        Log.d(TAG, String.format("endPosition x: %s, y: %s", endPosition.x, endPosition.y));
+        Log.d(TAG, "");
+
+        double xDistance = Math.abs(startPosition.x - endPosition.x);
+        double yDistance = Math.abs(startPosition.y - endPosition.y);
+
+        // Determine if trail was longer left/right or up/down to determine which letter to prioritize. (Useful if moving diagonally.)
+        if (xDistance > yDistance) {
+            if (endPosition.x < startPosition.x) {
+                // left
+                circleKeyboardApplication.commitText("L");
+            } else {
+                // right
+                circleKeyboardApplication.commitText("R");
+            }
+        } else {
+            if (endPosition.y < startPosition.y) {
+                // up
+                circleKeyboardApplication.commitText("U");
+            } else {
+                // down
+                circleKeyboardApplication.commitText("D");
+            }
+        }
     }
 
 }
