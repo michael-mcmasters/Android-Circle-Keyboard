52c3d363df4b0c4ff426a85701faacfb3ccc023a
Michael McMasters <jmcmasters411@gmail.com>
2024-01-27 02:06:44 -0500

    Enable entering U D L R when swiping in direction

--- app/src/main/java/com/example/sloppy_toppy_keyboard/ButtonListener.java
+++ app/src/main/java/com/example/sloppy_toppy_keyboard/ButtonListener.java
@@ -2,8 +2,11 @@ package com.example.sloppy_toppy_keyboard;
 
 import android.content.Context;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.core.view.MotionEventCompat;
 
 public class ButtonListener {
 
@@ -18,6 +21,10 @@ public class ButtonListener {
     private static final int firstRingActivationRange = 85;
     private static final int secondRingActivationRange = 200;
 
+    private Vector2 startPosition;
+    private Vector2 endPosition;
+
+
 
     public ButtonListener(Context context, CircleKeyboardApplication circleKeyboardApplication, MainKeyboardView mainKeyboardView) {
         this.context = context;
@@ -25,10 +32,46 @@ public class ButtonListener {
         this.mainKeyboardView = mainKeyboardView;
     }
 
+    // Gets start position
+    // Gets end position
+    // Determines if up/down/left/right
     public View.OnTouchListener getButtonCallback(Button button) {
         return (view, motionEvent) -> {
-            Log.d(TAG, "CALLED");
-            view.getHeight();
+            Integer fingerAction = MotionEventCompat.getActionMasked(motionEvent);
+            if (fingerAction.equals(MotionEvent.ACTION_DOWN)) {
+                Log.d(TAG, "ACTION_DOWN");
+                startPosition = new Vector2(motionEvent.getX(0), motionEvent.getY(0));  // should be relative to the button
+            } else if (fingerAction.equals(MotionEvent.ACTION_UP)) {
+                Log.d(TAG, "ACTION_UP");
+                endPosition = new Vector2(motionEvent.getX(0), motionEvent.getY(0));
+
+                Log.d(TAG, String.format("startPosition x: %s, y: %s", startPosition.x, startPosition.y));
+                Log.d(TAG, String.format("endPosition x: %s, y: %s", endPosition.x, endPosition.y));
+                Log.d(TAG, "");
+
+                double xDistance = Math.abs(startPosition.x - endPosition.x);
+                double yDistance = Math.abs(startPosition.y - endPosition.y);
+
+                // Determine if trail was longer left/right or up/down to determine which letter to prioritize. (Useful if moving diagonally.)
+                if (xDistance > yDistance) {
+                    if (endPosition.x < startPosition.x) {
+                        // left
+                        circleKeyboardApplication.commitText("L");
+                    } else {
+                        // right
+                        circleKeyboardApplication.commitText("R");
+                    }
+                } else {
+                    if (endPosition.y < startPosition.y) {
+                        // up
+                        circleKeyboardApplication.commitText("U");
+                    } else {
+                        // down
+                        circleKeyboardApplication.commitText("D");
+                    }
+                }
+            }
+
             view.performClick();
             return true;
         };
